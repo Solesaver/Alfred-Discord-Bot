e40287bb431329990f9d948ec2913eaec69aa391
Solesaver <saveursole@gmail.com>
2018-10-17 20:58:06 -0700

    fileio
    Alfred now responds to !howmany with how many times he's been asked that question, as well as how many times that user has done so.
    
    Alfred's commands are now case insensitive.
    
    Do not commit the Alfred's generated files.

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 console.log('import discord.io');
-var DiscordAPI = require('discord.io');
+const DiscordAPI = require('discord.io');
 console.log('import discordauth');
-var auth = require('./discordauth.js');
+const auth = require('./discordauth.js');
 console.log('import giphyauth');
-var giphy = require('./giphyauth.js');
+const giphy = require('./giphyauth.js');
+console.log('import path');
+const path = require('path');
+console.log('import fs');
+const fs = require('fs');
+
 
 console.log('creating and connecting bot');
 var bot = new DiscordAPI.Client({
@@ -12,10 +17,11 @@ var bot = new DiscordAPI.Client({
 });
 
 console.log('setting up commands');
-const commands = ['giphy', 'ping', 'help']
+const commands = ['giphy', 'ping', 'howmany', 'help']
 const execution = {
     giphy: CommandGiphy,
     ping: CommandPing,
+    howmany: CommandHowMany,
     help: CommandHelp
 }
 const help = {
@@ -23,7 +29,7 @@ const help = {
     help: "you're looking at it"
 };
 
-function CommandGiphy (user, userID, channelID, args) {
+function CommandGiphy (user, userId, channelId, args) {
     let phrase = args[1];
     if (args.length > 2) {
         for (let i = 2; i < args.length; ++i)
@@ -32,35 +38,98 @@ function CommandGiphy (user, userID, channelID, args) {
     giphy.translate('gifs', {"s": phrase})
         .then((response) => {
             bot.sendMessage({
-                to: channelID,
+                to: channelId,
                 message: response.data.embed_url
             });
         })
         .catch((err) => {
             bot.sendMessage({
-                to: channelID,
+                to: channelId,
                 message: 'Something went wrong with the giphy... thing...'
             });
         });
 }
 
-function CommandPing (user, userID, channelID, args) {
+function CommandPing (user, userId, channelId, args) {
     bot.sendMessage({
-        to: channelID,
+        to: channelId,
         message: 'pong'
     });
 }
 
-function CommandHelp (user, userID, channelID, args) {
+let howmanyObj = {};
+try {
+    howmanyObj = require('./data/howmany.json');
+}
+catch (ex) {
+    console.log('howmany.json does not exist');
+}
+
+function ensureDir (filepath) {
+    const dirname = path.dirname(filepath);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDir(dirname);
+    fs.mkdirSync(dirname);
+}
+
+function CommandHowMany (user, userId, channelId, args) {
+    let count = howmanyObj['total'];
+    let yourCount = howmanyObj[userId];
+    let countMessage = '';
+    if (!count) {
+        count = 1;
+        countMessage +=  "You're the first person to ask me that.";
+    }
+    else if (count == 1) {
+        countMessage += "I've only been asked that " + count + " other time.";
+        ++count;
+    }
+    else if (count < 10) {
+        countMessage += "I've only been asked that " + count + " other times.";
+        ++count;
+    }
+    else {
+        countMessage += "I've been asked that " + count + " times already.";
+        ++count;
+    }
+    howmanyObj['total'] = count;
+    if (!yourCount) {
+        yourCount = 1;
+        countMessage += " This is the first time you've asked."
+    }
+    else if (yourCount == 1) {
+        countMessage += " You've asked once before."
+        ++yourCount;
+    }
+    else if (yourCount < 10) {
+        countMessage += " You've only asked " + yourCount + " times before."
+        ++yourCount;
+    }
+    else {
+        countMessage += " You've already asked me that " + yourCount + " other times."
+        ++yourCount;
+    }
+    howmanyObj[userId] = yourCount;
     bot.sendMessage({
-        to: channelID,
+        to: channelId,
+        message: countMessage
+    });
+    ensureDir('./data/howmany.json');
+    fs.writeFileSync('./data/howmany.json', JSON.stringify(howmanyObj, null, 4), 'utf8');
+}
+
+function CommandHelp (user, userId, channelId, args) {
+    bot.sendMessage({
+        to: channelId,
         message: helpMessage
     });
 }
 
-function CommandDefault (user, userID, channelID, args) {
+function CommandDefault (user, userId, channelId, args) {
     bot.sendMessage({
-        to: channelID,
+        to: channelId,
         message: "'!" + args[0] + "' is not a command... yet?\nTry typing '!help' to see what's available."
     });
 }
@@ -84,15 +153,16 @@ for (const entry of commands) {
 helpMessage += '```'
 
 console.log('set up message callback');
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', function (user, userId, channelId, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         const args = message.substring(1).split(' ');
-        const command = args[0];
+        let command = args[0];
+        command = command.toLowerCase();
         let callback = execution[command];
         if (!callback)
             callback = CommandDefault;
-        callback(user, userID, channelID, args);
+        callback(user, userId, channelId, args);
     }
 });
